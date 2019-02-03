0e2e8e95d073cd27fd2a10ee2d0af1aff4bba44c
Alan Ionita <a.g.ionita@gmail.com>
2019-02-03 18:33:38 +0000

    adds final changes to the webgl section code

--- webgl.js
+++ webgl.js
@@ -1,76 +1,102 @@
-const canvasSketch = require('canvas-sketch');
-
-// Ensure ThreeJS is in global scope for the 'examples/'
 global.THREE = require('three');
 
-// Include any additional ThreeJS examples below
-require('three/examples/js/controls/OrbitControls');
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
+const eases = require('eases');
+const BezierEasing = require('bezier-easing');
 
 const settings = {
-  // Make the loop animated
   animate: true,
+  dimensions: [520, 520],
+  fps: 30,
+  duration: 10,
   // Get a WebGL canvas rather than 2D
   context: 'webgl',
   // Turn on MSAA
   attributes: { antialias: true }
 };
 
-const sketch = ({ context }) => {
+const sketch = ({ context, width, height }) => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
     context
   });
 
   // WebGL background color
-  renderer.setClearColor('#000', 1);
+  renderer.setClearColor('hsl(0, 0%, 95%)', 1);
 
-  // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
-  camera.lookAt(new THREE.Vector3());
-
-  // Setup camera controller
-  const controls = new THREE.OrbitControls(camera);
+  // Setup a camera, we will update its settings on resize
+  const camera = new THREE.OrthographicCamera();
 
   // Setup your scene
   const scene = new THREE.Scene();
 
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshPhysicalMaterial({
-      color: 'white',
-      roughness: 0.75,
-      flatShading: true
-    })
-  );
-  scene.add(mesh);
+  // Re-use the same Geometry across all our cubes
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-  // Specify an ambient/unlit colour
-  scene.add(new THREE.AmbientLight('#59314f'));
+  // Basic "unlit" material with no depth
 
-  // Add some light
-  const light = new THREE.PointLight('#45caf7', 1, 15.5);
-  light.position.set(2, 2, -4).multiplyScalar(1.5);
+  const palette = random.pick(palettes);
+  // Create the mesh
+  for (let i = 0; i < 100; i++) {
+    const material = new THREE.MeshStandardMaterial({
+      color: `${random.pick(palette)}`
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+    mesh.scale.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+    mesh.scale.multiplyScalar(0.5);
+    // Then add the group to the scene
+    scene.add(mesh);
+  }
+  scene.add(new THREE.AmbientLight('hsl(0, 0%, 50%)'));
+  const light = new THREE.DirectionalLight('white', 1);
+  light.position.set(0, 0, 4);
   scene.add(light);
+
+
+  const easeFn = BezierEasing(.18,.7,.31,.88);
 
   // draw each frame
   return {
     // Handle resize events here
-    resize ({ pixelRatio, viewportWidth, viewportHeight }) {
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
-      camera.aspect = viewportWidth / viewportHeight;
+
+      // Setup an isometric perspective
+      const aspect = viewportWidth / viewportHeight;
+      const zoom = 2;
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+      camera.near = -100;
+      camera.far = 100;
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update camera properties
       camera.updateProjectionMatrix();
     },
-    // Update & render your scene here
-    render ({ time }) {
-      mesh.rotation.y = time * (10 * Math.PI / 180);
-      controls.update();
+    // And render events here
+    render({ playhead }) {
+      // Draw scene with our camera
+      const t = Math.sin(playhead * Math.PI);
+      scene.rotation.y = easeFn(t);
       renderer.render(scene, camera);
     },
-    // Dispose of events & renderer for cleaner hot-reloading
-    unload () {
-      controls.dispose();
+    // Dispose of WebGL context (optional)
+    unload() {
       renderer.dispose();
     }
   };
