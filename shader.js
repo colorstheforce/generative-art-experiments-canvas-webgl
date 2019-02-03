6d2da95155d4e36ef38f1c60d89db88ec05826b6
Alan Ionita <a.g.ionita@gmail.com>
2019-02-03 21:32:42 +0000

    adds final code for shader lesson

--- shader.js
+++ shader.js
@@ -5,28 +5,35 @@ const glsl = require('glslify');
 // Setup our sketch
 const settings = {
   context: 'webgl',
-  animate: true
+  dimensions: [512, 512],
+  duration: 20,
+  animate: true,
+  frames: 24
 };
 
 // Your glsl code
 const frag = glsl(/* glsl */ `
   precision highp float;
 
-  uniform float time;
+  uniform float playhead;
   uniform float aspect;
   varying vec2 vUv;
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+  #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
 
   void main () {
-    // vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    vec3 colorA = sin(time) + vec3(1.0, 0.0, 0.0);
-    vec3 colorB = vec3(0.0, 1.0, 0.0);
-    vec3 color = mix(colorA, colorB, vUv.x + vUv.y * sin(time));
+    // vec3 color = 0.5 + 0.5 * cos(playhead + vUv.xyx + vec3(0.0, 2.0, 4.0));
+    // vec3 colorA = sin(playhead) + vec3(1.0, 0.0, 0.0);
+    // vec3 colorB = vec3(0.0, 1.0, 0.0);
+    // vec3 color = mix(colorA, colorB, vUv.x + vUv.y * sin(playhead));
 
     vec2 center = vUv - 0.5;
     center.x *= aspect;
     float dist = length(center);
-    float alpha = smoothstep(0.251 * sin(time), 0.25, dist);
-
+    float alpha = smoothstep(0.301, 0.3, dist);
+    
+    float n = noise(vec3(center * 2.5, playhead));
+    vec3 color = hsl2rgb((0.6 + n * 0.2), 0.5,0.5);
     gl_FragColor = vec4(color, alpha);
   }
 `);
@@ -43,7 +50,7 @@ const sketch = ({ gl }) => {
     // Specify additional uniforms to pass down to the shaders
     uniforms: {
       // Expose props from canvas-sketch
-      time: ({ time }) => time,
+      playhead: ({ playhead }) => Math.sin(2 * playhead * Math.PI),
       aspect: ({ width, height }) => width / height
     }
   });
